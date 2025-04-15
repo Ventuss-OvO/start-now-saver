9861cf5a704f7e2ba79d2cfce8a78a658f9f2c0d
Ventuss <36720963+Ventuss-OvO@users.noreply.github.com>
2025-04-15 17:43:19 +0800

    Initial Commit

---
+++ Start Now/Start_NowView.h
@@ -0,0 +1,12 @@
+//
+//  Start_NowView.h
+//  Start Now
+//
+//  Created by Jason on 4/15/25.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Start_NowView : ScreenSaverView
+
+@end
---
+++ Start Now/Start_NowView.m
@@ -0,0 +1,51 @@
+//
+//  Start_NowView.m
+//  Start Now
+//
+//  Created by Jason on 4/15/25.
+//
+
+#import "Start_NowView.h"
+
+@implementation Start_NowView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
